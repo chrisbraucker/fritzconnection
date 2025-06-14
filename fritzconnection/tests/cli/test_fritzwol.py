6f01d5b81121b006d9d7028de60b25ede3727e6e
Chris Bräucker <7303810+chrisbraucker@users.noreply.github.com>
2025-06-14 11:47:21 +0200

    Update tests for suggested changes

--- fritzconnection/tests/cli/test_fritzwol.py
+++ fritzconnection/tests/cli/test_fritzwol.py
@@ -8,21 +8,8 @@ def test_calls_wakeonlan_host_with_macaddress_directly():
     mac = 'C0:FF:EE:C0:FF:EE'
     fritz_host = Mock()
 
-    wake_host(fritz_host, Namespace(field='mac', host=mac))
+    wake_host(fritz_host, Namespace(device_mac_address=mac, device_ip_address=None, device_name=None))
     fritz_host.get_generic_host_entry.assert_not_called()
-    fritz_host.get_specific_host_entry_by_ip.assert_not_called()
-    fritz_host.get_generic_host_entries.assert_not_called()
-
-    fritz_host.wakeonlan_host.assert_called_with(mac)
-
-
-def test_n_calls_generic_host_then_wakeonlan():
-    mac = 'C0:FF:EE:C0:FF:EE'
-    fritz_host = Mock()
-    fritz_host.get_generic_host_entry.return_value = {'NewMACAddress': mac}
-
-    wake_host(fritz_host, Namespace(field='n', host='1'))
-    fritz_host.get_generic_host_entry.assert_called_with(0)
     fritz_host.get_specific_host_entry_by_ip.assert_not_called()
     fritz_host.get_generic_host_entries.assert_not_called()
 
@@ -34,13 +21,12 @@ def test_ip_calls_specific_host_then_wakeonlan():
     fritz_host = Mock()
     fritz_host.get_specific_host_entry_by_ip.return_value = {'NewMACAddress': mac}
 
-    wake_host(fritz_host, Namespace(field='ip', host='127.0.0.1'))
+    wake_host(fritz_host, Namespace(device_ip_address='127.0.0.1', device_mac_address=None, device_name=None))
     fritz_host.get_generic_host_entry.assert_not_called()
     fritz_host.get_specific_host_entry_by_ip.assert_called_with('127.0.0.1')
     fritz_host.get_generic_host_entries.assert_not_called()
 
     fritz_host.wakeonlan_host.assert_called_with(mac)
-
 
 
 def test_name_calls_generic_host_entries_then_wakeonlan():
@@ -51,7 +37,7 @@ def test_name_calls_generic_host_entries_then_wakeonlan():
         {'NewHostName': 'thishost', 'NewMACAddress': mac}
     ]
 
-    wake_host(fritz_host, Namespace(field='name', host='thishost'))
+    wake_host(fritz_host, Namespace(device_name='thishost', device_mac_address=None, device_ip_address=None))
     fritz_host.get_generic_host_entry.assert_not_called()
     fritz_host.get_specific_host_entry_by_ip.assert_not_called()
     fritz_host.get_generic_host_entries.assert_called_once()
