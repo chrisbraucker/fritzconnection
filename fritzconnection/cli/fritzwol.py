b912ef882fb429cee42e22db66a0b9bc49eb9c5a
Chris Bräucker <7303810+chrisbraucker@users.noreply.github.com>
2025-06-14 11:47:36 +0200

    Adapt CLI API to suggested changes

--- fritzconnection/cli/fritzwol.py
+++ fritzconnection/cli/fritzwol.py
@@ -3,7 +3,7 @@ fritzwol.py
 
 Module to wake up a single host via the Fritzbox built-in mechanism.
 This can be helpful if the host to be woken up is in a different
-broadcast domain/ subnet than the client which tries to wake up.
+broadcast domain/subnet than the client which tries to wake up.
 CLI interface.
 
 This module is part of the FritzConnection package.
@@ -12,17 +12,19 @@ License: MIT (https://opensource.org/licenses/MIT)
 Authors: Maik Töpfer, Chris Bräucker
 """
 
-from fritzconnection.core.exceptions import FritzArgumentError, FritzArrayIndexError, FritzAuthorizationError, FritzLookUpError
-from ..lib.fritzhosts import FritzHosts
-from . utils import (
-    get_cli_arguments,
-    get_instance,
-    print_header,
-    print_common_exception_message
+from fritzconnection.core.exceptions import (
+    FritzArgumentError,
+    FritzAuthorizationError,
+    FritzLookUpError,
 )
 
-class DeviceUnknownException(Exception):
-    """Exception raised if the reference to the host does not resolve."""
+from ..lib.fritzhosts import FritzHosts
+from .utils import (
+    get_cli_arguments,
+    get_instance,
+    print_common_exception_message,
+    print_header,
+)
 
 
 def wake_host(fh, args):
@@ -30,48 +32,52 @@ def wake_host(fh, args):
     Either wakes a host directly by MAC address, which should even work for hosts not known.
     Or it tries to find the given parameter in the device list to determine the MAC address.
     """
-    mac = args.host
 
-    if args.field == 'n':
+    print(args)
+
+    if args.device_mac_address:
+        mac = args.device_mac_address
+
+    elif args.device_ip_address:
         try:
-            host = fh.get_generic_host_entry(int(args.host) - 1)
-        except (FritzArgumentError, FritzArrayIndexError) as err:
-            raise DeviceUnknownException("The index provided is invalid", args.host)
+            host = fh.get_specific_host_entry_by_ip(args.device_ip_address)
+        except (FritzArgumentError, FritzLookUpError):
+            msg = f"Error: unknown IP {args.device_ip_address}"
+            raise FritzArgumentError(msg)
         mac = host['NewMACAddress']
 
-    elif args.field == 'ip':
-        try:
-            host = fh.get_specific_host_entry_by_ip(args.host)
-        except (FritzArgumentError, FritzLookUpError) as err:
-            raise DeviceUnknownException("The IP provided is unknown", args.host)
-        mac = host['NewMACAddress']
-
-    elif args.field == 'name':
-        found = False
+    elif args.device_name:
+        device_name = args.device_name.lower()
         for entry in fh.get_generic_host_entries():
-            if entry['NewHostName'].lower() == args.host.lower():
+            if entry['NewHostName'].lower() == device_name:
                 mac = entry['NewMACAddress']
-                found = True
                 break
-
-        if not found:
-            raise DeviceUnknownException("The hostname provided is unknown", args.host)
+        else:
+            msg = f"Error: unknown device name '{args.device_name}'"
+            raise FritzArgumentError(msg)
 
     fh.wakeonlan_host(mac)
     print(f"Waking {mac}")
 
 
-
 def add_arguments(parser):
-    parser.add_argument('field',
-                        choices=('ip', 'name', 'mac', 'n'),
-                        default='mac',
-                        nargs='?',
-                        help='Which host field to wake by. ' +
-                        'Retrieve this data with the `fritzhosts` command. ' +
-                        '\'mac\' sends the WoL package directly, without checking. ' +
-                        '(default: mac)')
-    parser.add_argument('host', help='Field value of host to be woken up')
+    arggroup = parser.add_argument_group('Wake-on-LAN options', 'mutually exclusive arguments to reference the target host')
+    group = arggroup.add_mutually_exclusive_group(required=True)
+    group.add_argument('-m', '--device-mac',
+                        dest='device_mac_address',
+                        default=None,
+                        help='MAC address of the device to wake up. Sends a WoL packet without checking device existence.'
+    )
+    group.add_argument('-I', '--device-ip',
+                        dest='device_ip_address',
+                        default=None,
+                        help='IP address of the device to wake up. Checks against the list of known devices.'
+    )
+    group.add_argument('-n', '--device-name',
+                        dest='device_name',
+                        default=None,
+                        help='Name of the device to wake up. Checks against the list of known devices.'
+    )
 
 
 def execute():
@@ -86,10 +92,8 @@ def main():
         execute()
     except FritzAuthorizationError as err:
         print_common_exception_message(err)
-    except FritzArgumentError:
-        print(f"Error: Invalid MAC address format")
-    except DeviceUnknownException as err:
-        print(f"Error: {err.args[0]}: {err.args[1]}")
+    except FritzArgumentError as err:
+        print(err)
 
 
 if __name__ == '__main__':
